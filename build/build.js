50318ef06477e458cbca9f5ef33c5a38efbfed13
Quentin <hausser.quentin@gmail.com>
2020-05-01 01:23:30 +0200

    trigger ci :bow:

--- build/build.js
+++ build/build.js
@@ -20,20 +20,24 @@ shell.config.silent = true;
 shell.cp('-R', 'static/*', assetsPath);
 shell.config.silent = false;
 
-webpack(webpackConfig, (err, stats) => {
-  spinner.stop();
-  if (err) throw err;
-  process.stdout.write(`${stats.toString({
-    colors: true,
-    modules: false,
-    children: false,
-    chunks: false,
-    chunkModules: false,
-  })}\n\n`);
+try {
+  webpack(webpackConfig, (err, stats) => {
+    spinner.stop();
+    if (err) throw err;
+    process.stdout.write(`${stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false,
+    })}\n\n`);
 
-  console.log(chalk.cyan('  Build complete.\n'));
-  console.log(chalk.yellow(
-    '  Tip: built files are meant to be served over an HTTP server.\n'
+    console.log(chalk.cyan('  Build complete.\n'));
+    console.log(chalk.yellow(
+      '  Tip: built files are meant to be served over an HTTP server.\n'
     + '  Opening index.html over file:// won\'t work.\n',
-  ));
-});
+    ));
+  });
+} catch (e) {
+  console.log(chalk.red('  Build failure.\n'));
+}
